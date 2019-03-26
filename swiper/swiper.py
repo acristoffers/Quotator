528ebf618e93fa836711c610477ce0fa165ef49b
Álan Crístoffer <acristoffers@gmail.com>
2019-03-26 11:32:16 -0300

    Fix resolution to 600x600

--- swiper/swiper.py
+++ swiper/swiper.py
@@ -40,8 +40,8 @@ def print_file(user, file):
     copies = 1
     fout = os.path.dirname(file) + '/' + uuid.uuid4().hex + '.ps'
     gsopts = [
-        '-dNOPAUSE', '-dBATCH', '-sDEVICE=ps2write', '-sPAPERSIZE=a4',
-        '-sOutputFile=' + fout
+        '-dNOPAUSE', '-dBATCH', '-sDEVICE=ps2write', '-rXRESxYRES=600x600',
+        '-sPAPERSIZE=a4', '-sOutputFile=' + fout
     ]
     cmd = ['/sbin/runuser', '-u', user, '--', '/usr/bin/gs', *gsopts, file]
     p = subprocess.Popen(cmd)
